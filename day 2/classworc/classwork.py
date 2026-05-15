30b8f9641dc080dae1bd3cc023f81dc8c75a2d38
iliabokuchava <iliabokuchava74@gmail.com>
2026-05-15 22:35:47 +0400

    second commit

--- day 2/classworc/classwork.py
+++ day 2/classworc/classwork.py
@@ -1,7 +1,7 @@
 num1 = 24     #ავიღეთ პირველი რიცხვი
 num2 = 2      #ავიღეთ მეორე რიცხვი
 
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
+print(num1 + num2)   #num1 მივუმატეთ num2
+print(num1 - num2)   #num1 გამოვაკლეთ num2
+print(num1 * num2)   #num1 გავამრავლეთ num2
+print(num1 / num2)   #num1 გავხავით num2
---
+++ day 3/homework/homework.py
@@ -0,0 +1,12 @@
+# N1
+num1 = 36
+num2 = 12
+num3 = 2
+
+print(num1 - num2 - num3)
+print(num1 + num2 + num3)
+print(num1 / num2 / num3)
+print(num1 // num2 // num3)
+print(num1 * num2 * num3)
+print(num1 ** num2 ** num3)
+print(num1 % num2 % num3)
