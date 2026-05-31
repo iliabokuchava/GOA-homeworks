5b399a062cdbf0dabaae5a244ba195598b709ca4
iliabokuchava <iliabokuchava74@gmail.com>
2026-05-31 15:58:18 +0400

    second commit

---
+++ day 5.5/classwork/classwork.py
@@ -0,0 +1,30 @@
+name = 'ilia'
+lastname = 'bokuchava'
+age = 13
+city = 'zugdid'
+
+print(f'{name} {lastname} {age} {city}')
+
+x = 25
+y = 4
+
+print(x + y)
+print(x - y)
+print(x / y)
+print(x // y)
+print(x * y)
+print(x ** y)
+print(x % y)
+
+status = 'user'
+print(status)
+
+status = 'admin'
+print(status)
+
+color = input('შეიყვანე საყვარელი ფერი')
+
+print('შენი საყვარელი ფერია' + color)
+
+#snace_case - ის დახმარებით შეგვიძლია ორშიტყვიანი სტრინგი შევქმნათ _
+#pyton არის case_sensitive, ეს ნიშნავს იმარ რომ pyton-ის თვის დიდსა და პატარა ასოებში განსხვავებას ხედავსშ
---
+++ day 5.5/homework/homework.py
@@ -0,0 +1,15 @@
+name = input('შემოიყვანე შენი სახელი')
+print(f'hello {name}')
+
+num1 = input()
+num2 = input()
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 * num2)
+print(num1 ** num2)
+print(num1 % num2)
+
+#2 დავალება არ მუშაობს იმიტომ რომ input()-ი ინფორმაციას სტრინგის სახით, და სტრინგების გამოკლება არ შეიძლება
