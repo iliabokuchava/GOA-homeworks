d12b485a85fde12f9a1e4727697870ff15503347
iliabokuchava <iliabokuchava74@gmail.com>
2026-06-01 12:30:53 +0400

    second commit

---
+++ day 6/classwork/classwork.py
@@ -0,0 +1,18 @@
+#data conversation - არის მონაცემთა ტიპების გარდაქმნა, გვაქვს 3 ტიპი:int, str და float-ი
+
+num = "20"
+num1 = int(num)
+num2 = float(num)
+
+age = "15"
+
+new_age = int(age)
+
+print(type(new_age))
+#ამ კოდში პირველად '15' გარდაიქმნება 15, მერე მონაცემთ ტიპის შემოწმება მიდის,
+
+
+
+
+
+
---
+++ day 6/homework/homework.py
@@ -0,0 +1,38 @@
+#1
+#str() - ფუნქციით შეგვიძლია int-ი და float-ი გარდავქმნათ str-ად
+Num1 = 25.6
+Num2 = 7
+
+num1 = str(Num1)
+num2 = str(Num2)
+
+#int() - ფუნქციით შეგვიძლია გარდავქმნათ მარტო str-ი int-ად
+Num3 = '52'
+
+num3 = int(Num3)
+
+#float() - ფუნქციით შეგვიძლია int და str გარდავქმნათ sloat-ად
+Num4 = 57
+Num5 = '62'
+
+num4 = float(Num4)
+num5 = float(Num5)
+
+#2
+#Data - ნიშნავს მონაცემთა ტიპს, ჩვენ ვიცით 3 სახის მონაცემთა ტიპი: integer-ი, string-ი და float-ი
+
+#3
+print("10" + "20")
+#ამ კოდში პასუხი გამოვა 1020 იმიტომ რომ, სტრინგების მათემათიკურად ეთმანეთთან მიმატება არ შეიძლება, ამიტომ აქ მოხვდა კონკატენაცია.
+
+#4S
+number1 = int(input('შეიყვანე პირველი რიცზვი: '))
+number2 = int(input('შემოიყვანე მეორე რიცხვი: '))
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 / number2)
+print(number1 // number2)
+print(number1 * number2)
+print(number1 ** number2)
+print(number1 % number2)
