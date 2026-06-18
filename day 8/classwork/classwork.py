59600d4f2f1ccaf8cc0624cbec3b03c07bd3e972
iliabokuchava <iliabokuchava74@gmail.com>
2026-06-18 20:40:20 +0400

    second commit

---
+++ day 8/classwork/classwork.py
@@ -0,0 +1,19 @@
+#boolean - არის მონაცემთა ტიპი, რომელიც შედგება fals და true-ს გან, ესენი არიან შედარების ოპერატორების პასუხი
+
+age = 10
+print(age >= 12)
+
+score = 50
+print(score >= 50)
+
+knows_coding = True
+knows_english = False
+print(knows_coding or knows_english)
+
+is_sunny = False
+is_cloudy = True
+print(is_sunny or is_cloudy)
+
+secret_number = 7
+player_guess = 9
+print(secret_number == player_guess)
---
+++ day 8/homework/homework.py
@@ -0,0 +1,20 @@
+age = 16
+print(age >= 18)
+
+score = 65
+attendance = 80
+print(score > 50 and attendance > 70)
+
+user_age = 10
+print(user_age < 12 or user_age > 65)
+
+x = 14
+print(x > 0)
+
+correct_user = "admin"
+correct_pass = "1234"
+
+entered_user = "admin"
+enteredrrect_pass = "1234"
+
+print(correct_user == entered_user and enteredrrect_pass==correct_pass)
