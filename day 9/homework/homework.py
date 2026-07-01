8b4fb95863bec3cbd52cd49101b34ea7f2c4a6c0
iliabokuchava <iliabokuchava74@gmail.com>
2026-07-01 23:21:29 +0400

    second commit

---
+++ day 9/homework/homework.py
@@ -0,0 +1,20 @@
+for i in range(100):
+    print("ilia")
+
+for i in range(1,101):
+    print(f'hello wirld{i}')
+
+total_sum = 0
+for i in range(9, 100):
+    total_sum += i
+print(total_sum)
+
+name = 'ilia'
+for name in name:
+    print(name)
+
+for i in range(2, 1001, 2):
+    print(i)
+
+for i in range(4, 501, 3):
+    print(i)
