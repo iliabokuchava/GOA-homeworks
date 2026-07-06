3815cbfc8f0cd96b901a60a4cec11febce6a0d74
iliabokuchava <iliabokuchava74@gmail.com>
2026-07-06 20:37:47 +0400

    second commit

---
+++ day 9.5/homework/bank.py
@@ -0,0 +1,8 @@
+balance = int(input('შემოიყვანე შენი ბალანსი: '))
+transfer = int(input('შემოიყვანე გადასარიცხი ფული:'))
+total_balance = balance - transfer
+
+print('------------------------')
+print(f"თქვენი ბალანსი იყო {balance}")
+print(f"თქვენ მიერ გადარიცხული თანხაა {transfer}")
+print(f"ბალანსზეა დარჩენილი თანხაა {total_balance}")
---
+++ day 9.5/homework/classwork1.py
@@ -0,0 +1,17 @@
+#N1
+lastname = input('შემოიყვანე შენი გავრი: ')
+for i in lastname:
+    print(i)
+
+#N2
+for i in range(50, 201):
+    print(i)
+
+#N3
+for i in range(1, 101, 2):
+    print(i)
+
+#N4
+for i in range(100, 151, 3):
+    print(i)
+
